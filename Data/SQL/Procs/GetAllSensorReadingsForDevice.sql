4a136b0f63d136f5f9f445d243c4c0a4e9535c49
davesanderman <dave-github@sanderman.org>
2026-07-26 10:11:52 -0700

    use FETCH NEXT instead of ROWCOUNT with paged queries

--- Data/SQL/Procs/GetAllSensorReadingsForDevice.sql
+++ Data/SQL/Procs/GetAllSensorReadingsForDevice.sql
@@ -21,13 +21,21 @@ BEGIN
 	END
 
 	IF @readingCount IS NOT NULL
-		SET ROWCOUNT @readingcount
-	SELECT * from SensorReadings
-		WHERE DeviceId = @deviceId
-		AND (@fromTime IS NULL OR Timestamp >= @fromTime)
-		AND (@toTime IS NULL OR Timestamp <= @toTime)
-		ORDER BY Timestamp DESC
-		OFFSET (@skipCount) ROWS
-	SET ROWCOUNT 0
+
+        SELECT * from SensorReadings
+            WHERE DeviceId = @deviceId
+            AND (@fromTime IS NULL OR Timestamp >= @fromTime)
+            AND (@toTime IS NULL OR Timestamp <= @toTime)
+            ORDER BY Timestamp DESC
+            OFFSET (@skipCount) ROWS
+            FETCH NEXT @readingCount ROWS ONLY
+    ELSE
+        SELECT * from SensorReadings
+            WHERE DeviceId = @deviceId
+            AND (@fromTime IS NULL OR Timestamp >= @fromTime)
+            AND (@toTime IS NULL OR Timestamp <= @toTime)
+            ORDER BY Timestamp DESC
+            OFFSET (@skipCount) ROWS
+
 END
 GO
